e3273007663e1fe37dd1b15a308f34324804fb6d
FabTrindade <fabrtrindade@gmail.com>
2023-02-21 16:28:14 -0300

    Getting started with Models class (NotePage)

--- Notes/Views/NotePage.xaml.cs
+++ Notes/Views/NotePage.xaml.cs
@@ -7,8 +7,10 @@ public partial class NotePage : ContentPage
 	{
 		InitializeComponent();
 
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
 
     private void SaveButton_Clicked(object sender, EventArgs e)
@@ -25,4 +27,18 @@ public partial class NotePage : ContentPage
 
         TextEditor.Text = string.Empty;
     }
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
+    }
 }
+
